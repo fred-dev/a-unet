5bbf35bd0a8ce1d7be2e3f36888ceb5dde8b8f5e
“fred-dev” <fredaudio@gmail.com>
2023-10-06 13:50:21 +1100

    Tabular Embedder

--- a_unet/blocks.py
+++ a_unet/blocks.py
@@ -629,6 +629,8 @@ def MultiCCVariableConditioningPlugin(
     net_t: Type[nn.Module],
 ) -> Callable[..., nn.Module]:
     """Adds text conditioning without an embedder"""
+    embedder = embedder if exists(embedder) else T5Embedder()
+
     def Net(embedding_features: int, **kwargs) -> nn.Module:
         net = net_t(embedding_features=embedding_features, **kwargs)  # type: ignore
 
@@ -690,3 +692,92 @@ def ClassifierFreeGuidancePluginCCVariables(
         return Module([fixed_embedding, net], forward)
 
     return Net
+
+#this is the embedding function for tabular data from tabnet https://github.com/dreamquark-ai/tabnet/blob/2c0c4ebd2bb1cb639ea94ab4b11823bc49265588/pytorch_tabnet/tab_network.py#L809
+class EmbeddingGenerator(torch.nn.Module):
+    """
+    Classical embeddings generator
+    """
+
+    def __init__(self, input_dim, cat_dims, cat_idxs, cat_emb_dims, group_matrix):
+        """This is an embedding module for an entire set of features
+
+        Parameters
+        ----------
+        input_dim : int
+            Number of features coming as input (number of columns)
+        cat_dims : list of int
+            Number of modalities for each categorial features
+            If the list is empty, no embeddings will be done
+        cat_idxs : list of int
+            Positional index for each categorical features in inputs
+        cat_emb_dim : list of int
+            Embedding dimension for each categorical features
+            If int, the same embedding dimension will be used for all categorical features
+        group_matrix : torch matrix
+            Original group matrix before embeddings
+        """
+        super(EmbeddingGenerator, self).__init__()
+
+        if cat_dims == [] and cat_idxs == []:
+            self.skip_embedding = True
+            self.post_embed_dim = input_dim
+            self.embedding_group_matrix = group_matrix.to(group_matrix.device)
+            return
+        else:
+            self.skip_embedding = False
+
+        self.post_embed_dim = int(input_dim + np.sum(cat_emb_dims) - len(cat_emb_dims))
+
+        self.embeddings = torch.nn.ModuleList()
+
+        for cat_dim, emb_dim in zip(cat_dims, cat_emb_dims):
+            self.embeddings.append(torch.nn.Embedding(cat_dim, emb_dim))
+
+        # record continuous indices
+        self.continuous_idx = torch.ones(input_dim, dtype=torch.bool)
+        self.continuous_idx[cat_idxs] = 0
+
+        # update group matrix
+        n_groups = group_matrix.shape[0]
+        self.embedding_group_matrix = torch.empty((n_groups, self.post_embed_dim),
+                                                  device=group_matrix.device)
+        for group_idx in range(n_groups):
+            post_emb_idx = 0
+            cat_feat_counter = 0
+            for init_feat_idx in range(input_dim):
+                if self.continuous_idx[init_feat_idx] == 1:
+                    # this means that no embedding is applied to this column
+                    self.embedding_group_matrix[group_idx, post_emb_idx] = group_matrix[group_idx, init_feat_idx]  # noqa
+                    post_emb_idx += 1
+                else:
+                    # this is a categorical feature which creates multiple embeddings
+                    n_embeddings = cat_emb_dims[cat_feat_counter]
+                    self.embedding_group_matrix[group_idx, post_emb_idx:post_emb_idx+n_embeddings] = group_matrix[group_idx, init_feat_idx] / n_embeddings  # noqa
+                    post_emb_idx += n_embeddings
+                    cat_feat_counter += 1
+
+    def forward(self, x):
+        """
+        Apply embeddings to inputs
+        Inputs should be (batch_size, input_dim)
+        Outputs will be of size (batch_size, self.post_embed_dim)
+        """
+        if self.skip_embedding:
+            # no embeddings required
+            return x
+
+        cols = []
+        cat_feat_counter = 0
+        for feat_init_idx, is_continuous in enumerate(self.continuous_idx):
+            # Enumerate through continuous idx boolean mask to apply embeddings
+            if is_continuous:
+                cols.append(x[:, feat_init_idx].float().view(-1, 1))
+            else:
+                cols.append(
+                    self.embeddings[cat_feat_counter](x[:, feat_init_idx].long())
+                )
+                cat_feat_counter += 1
+        # concat
+        post_embeddings = torch.cat(cols, dim=1)
+        return post_embeddings
